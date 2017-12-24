b1706b8d5852823ea5723b26a1f8c2888db4442a
Blaine Miller <blaine@blaineam.com>
2017-12-23 21:42:42 -0800

    swift 4

--- HDAugmentedReality/Classes/ARAnnotationView.swift
+++ HDAugmentedReality/Classes/ARAnnotationView.swift
@@ -8,12 +8,30 @@
 
 import UIKit
 
-/// View for annotation. Subclass to customize. Annotation views should be lightweight,
-/// try to avoid xibs and autolayout.
-/// bindUi method is called when distance/azimuth is set in ARViewController.
+/**
+ Responsible for presenting annotations visually. Analogue to MKAnnotationView. 
+ It is usually subclassed to provide custom look.
+ 
+ Annotation views should be lightweight, try to avoid xibs and autolayout.
+ */
 open class ARAnnotationView: UIView
 {
+    //===== Public
+    /** 
+     Normally, center of annotationView points to real location of POI, but this property can be used to alter that.
+     E.g. if bottom-left edge of annotationView should point to real location, centerOffset should be (0, 1)
+     */
+    open var centerOffset = CGPoint(x: 0.5, y: 0.5)
+    /// Annotation associated with this view.
     open weak var annotation: ARAnnotation?
+
+    /// Position of annotation view without heading, pitch, stack offsets.
+    open var arPosition: CGPoint = CGPoint(x: 0, y: 0)
+    /// Offset for arPosition, this is not offset for heading/pitch but offset that is set in ARPresenterTransform.
+    open var arPositionOffset = CGPoint(x: 0, y: 0)
+    /// Used for internal calculations.
+    open var arAlternateFrame: CGRect = CGRect.zero
+
     fileprivate var initialized: Bool = false
     
     public init()
@@ -60,4 +78,6 @@ open class ARAnnotationView: UIView
     {
         
     }
+    
+
 }
